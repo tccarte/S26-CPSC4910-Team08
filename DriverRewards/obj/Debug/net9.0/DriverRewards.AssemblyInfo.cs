e0da3fcebea376a09ea167f4b758f56c1f517264
Will Cooper <wwcoope@g.clemson.edu>
2026-02-10 15:22:43 -0500

    na

--- DriverRewards/obj/Debug/net9.0/DriverRewards.AssemblyInfo.cs
+++ DriverRewards/obj/Debug/net9.0/DriverRewards.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriverRewards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9bade798f39486593fa45ee253754dd3c624e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820a358874295c0bec746b3920c00eab127c34c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriverRewards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriverRewards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
